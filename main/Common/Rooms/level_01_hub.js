ce1d8b5155b7f02418ad9f5681ac0b0d7d684b54
ctuimolo <ctuimolo@ucsc.edu>
2015-02-27 01:20:34 -0800

    Prototype 0.8
    + Can highlight notes and get a "my notes" message
    + added some sprites for the icons
    
    - in need of more target screens
    - in need of notes

--- main/Common/Rooms/level_01_hub.js
+++ main/Common/Rooms/level_01_hub.js
@@ -18,15 +18,42 @@ var level_01_hub = new Sprite();
 		level_01_hub.image_sniper.y = 0;
 		level_01_hub.image_sniper.image = Textures.load
 			("./Common/Textures/sniper.png");
-		
+			
+		level_01_hub.image_comment = new TextBox("\"my notes\"");
+		level_01_hub.image_comment.font = "Courier";
+		level_01_hub.image_comment.fontSize = 30;
+		level_01_hub.image_comment.color = "#FFFFFF";
+		level_01_hub.image_comment.x = 800;
+		level_01_hub.image_comment.y = 680;
+		level_01_hub.image_comment.visible = false;
 		
 		// This room's active sprites
+		level_01_hub.button_notes = new Sprite();
+		level_01_hub.button_notes.width  = 446;
+		level_01_hub.button_notes.height = 182;
+		level_01_hub.button_notes.x = 50;
+		level_01_hub.button_notes.y = 540;
+		level_01_hub.button_notes.mouseOver = false;
+		level_01_hub.button_notes.update = function() {
+			if(level_01_hub.button_notes.mouseOver) {
+				level_01_hub.button_notes.image = Textures.load
+					("./Common/Textures/notes over.gif");
+				level_01_hub.image_comment.visible = true;
+			}else {
+				level_01_hub.button_notes.image = Textures.load
+					("./Common/Textures/notes up.png");
+				level_01_hub.image_comment.visible = false;
+			}
+		}
 		
 		// Visible sprites at creation time
 		world.addChild(level_01_hub.image_background);
 		world.addChild(level_01_hub.image_sniper);
+		world.addChild(level_01_hub.image_comment);
+		world.addChild(level_01_hub.button_notes);
 		
 		// Active sprites at creation time
+		active_sprites.push(level_01_hub.button_notes);
 	}
 	
 	// Clear this room
